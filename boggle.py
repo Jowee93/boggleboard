1f2371fb5966fac4f371739f96e1efe408bdd1e1
Jowee93 <yeapjowee@gmail.com>
2019-10-28 13:35:54 +0800

    Clean up code, added end_game criteria, score and continous loop until end game using 'exit!'

--- boggle.py
+++ boggle.py
@@ -8,28 +8,30 @@ boggle_board = [
     ["-","-","-","-"],
     ["-","-","-","-"]
 ]
+######## Part 1 : Not so smart boggle board ######## 
+    # boggle_board = [
+    #     ["Z","F","L","S"],
+    #     ["E","H","N","W"],
+    #     ["B","A","U","Y"],
+    #     ["H","I","I","H"]
+    # ]
 
-# boggle_board = [
-#     ["Z","F","L","S"],
-#     ["E","H","N","W"],
-#     ["B","A","U","Y"],
-#     ["H","I","I","H"]
-# ]
 
-
-##### Part 1 : Not so smart boggle board #### 
-
-# def print_boggle_board() :
     
-#     for i in range (0, 4) :
-#         for j in range (0,4) :
-#             boggle_board[i][j] = random.choice(string.ascii_uppercase)
-            
-#     for rows in boggle_board :
-#         print(' '.join(rows))
-  
-  
-# print_boggle_board()
+
+    # def print_boggle_board() :
+        
+    #     for i in range (0, 4) :
+    #         for j in range (0,4) :
+    #             boggle_board[i][j] = random.choice(string.ascii_uppercase)
+                
+    #     for rows in boggle_board :
+    #         print(' '.join(rows))
+    
+    
+    # print_boggle_board()
+
+####################################################
 
 
 
@@ -54,7 +56,10 @@ dice = [
     ['D', 'E', 'I', 'L', 'R', 'X']
 ]
         
+
+# --------------------------------
 ##### Shuffle the boggle board ####
+# --------------------------------
 def shuffle_smarter_boggle_board() :    
     
     # random shuffle
@@ -73,7 +78,10 @@ def shuffle_smarter_boggle_board() :
         
 shuffle_smarter_boggle_board()
 
+
+# --------------------------------
 ##### Print the boggle board in 4 x 4 #####
+# --------------------------------
 def print_smarter_boggle_board() :
     
     print("--------------")
@@ -88,268 +96,224 @@ def print_smarter_boggle_board() :
            
 print_smarter_boggle_board()
 
+
+# --------------------------------
 ##### Start the game by giving word #####
-# Ask for input
+# --------------------------------
 
-word = input("Enter Word : \n").upper()
+end_game = False
+score = 0
 
-while len(word) <= 1 :
-    print_smarter_boggle_board()
-    word = input("Enter more than 1 character ! : \n").upper()
-
-word_list = list(word)
-all_first_position = []
-first_char_horizontal = 0
-first_char_vertical = 0
-
-##### Find first letter position in boggle board #####
-def get_first_char() :  
+while end_game == False :
+    # Ask for input
+    print("Type 'exit!' to exit the game")
+    word = input("Enter Word : \n").upper()
     
-    for i in range(0, 1) :
-        for j in range(0, len(boggle_board)) :
-            for k in range(-1, len(boggle_board[j])) :    
-                try :
-                    if boggle_board[j].index(word_list[i],k + 1) >= 0 :
-                        first_char_horizontal = boggle_board[j].index(word_list[i],k + 1)
-                        first_char_vertical = j
-                        position = [first_char_vertical,first_char_horizontal]
-                        all_first_position.append(position)
-                        # print(f"k is : {k + 1}")
-                        # print(f"index is : {boggle_board[j].index(word_list[i],k + 1)}")
-                except :
-                    continue
-    
-    return all_first_position
-    
-get_first_char()
-
-##### Turn first characters to a list and make it unique #####
-unique_all_first_position = [list(item) for item in set(tuple(row) for row in all_first_position)]
-
-
-##### Check horizontal axis #####
-def checkHorizontal() :
-    
-    match = False
-    
-    # check horizontal from left to right
-    for i in range(0, len(unique_all_first_position)) :
-        if match == True :
-            return True
-        else :    
-            for j in range(1, len(word_list)) :
-                try :
-                    if boggle_board[unique_all_first_position[i][0]][unique_all_first_position[i][1] + j] == word_list[j] :
-                        match = True
-                    else :
-                        match = False
-                        break
-                        
-                except :
-                    match = False
-                         
-    # check horizontal from right to left
-    for i in range(0, len(unique_all_first_position)) :
-        if match == True :
-            return True
-        else :    
-            for j in range(1, len(word_list)) :
-                try:
-                    if boggle_board[unique_all_first_position[i][0]][unique_all_first_position[i][1] - j] == word_list[j] :
-                        match = True
-                    else :
-                        match = False
-                        break
-                except: 
-                    match = False
-                    
-    if match == False :
-        return False
-    else :
-        return True
+    if word == "EXIT!" :
+        end_game = True
+        continue
         
-print(f"Horizontal: {checkHorizontal()}")
+    while len(word) <= 1 :
+        print_smarter_boggle_board()
+        word = input("Enter more than 1 character ! : \n").upper()
 
-def checkVertical () :
-    match = False
-    
-    # check horizontal from left to right
-    for i in range(0, len(unique_all_first_position)) :
-        if match == True :
-            return True
-        else :    
-            for j in range(1, len(word_list)) :
-                try :
-                    if boggle_board[unique_all_first_position[i][0] + j][unique_all_first_position[i][1]] == word_list[j] :
-                        match = True
-                    else :
-                        match = False
-                        break
-                        
-                except :
-                    match = False
-                         
-    # check horizontal from right to left
-    for i in range(0, len(unique_all_first_position)) :
-        if match == True :
-            return True
-        else :    
-            for j in range(1, len(word_list)) :
-                try:
-                    if boggle_board[unique_all_first_position[i][0] - j][unique_all_first_position[i][1]] == word_list[j] :
-                        match = True
-                    else :
-                        match = False
-                        break
-                except: 
-                    match = False
-                    
-    if match == False :
-        return False
-    else :
-        return True
-    
-print(f"Vertical: {checkVertical()}")
+    word_list = list(word)
+    all_first_position = []
+    first_char_horizontal = 0
+    first_char_vertical = 0
 
-print(f"First position are : {unique_all_first_position}")
-print(f"Word provided is : {word_list}")
+    ##### Find first letter position in boggle board #####
+    def get_first_char() :  
+        
+        for i in range(0, 1) :
+            for j in range(0, len(boggle_board)) :
+                for k in range(-1, len(boggle_board[j])) :    
+                    try :
+                        if boggle_board[j].index(word_list[i],k + 1) >= 0 :
+                            first_char_horizontal = boggle_board[j].index(word_list[i],k + 1)
+                            first_char_vertical = j
+                            position = [first_char_vertical,first_char_horizontal]
+                            all_first_position.append(position)
+                            # print(f"k is : {k + 1}")
+                            # print(f"index is : {boggle_board[j].index(word_list[i],k + 1)}")
+                    except :
+                        continue
+        
+        return all_first_position
+        
+    get_first_char()
+
+    ##### Turn first characters to a list and make it unique #####
+    unique_all_first_position = [list(item) for item in set(tuple(row) for row in all_first_position)]
 
 
-##### Check all 8 directions #####
-
-directions = [
-    [+1, +0], #down
-    [-1, +0], #up
-    [+0, +1], #right
-    [+0, -1], #left
-    [+1, +1], #down right
-    [-1, +1], #up right
-    [-1, -1], #up left
-    [+1, -1] #down left
-]
-
-# print(directions)
-
-
-
-# def checkAllDir() :
-#     match = False
-#     matches = []
-    
-#     # check horizontal from left to right
-#     for i in range(0, len(unique_all_first_position)) :
-#         if match == True :
-#             return True
-#         else :    
-#             for j in range(1, len(word_list)) :
-#                 for k in range(0, len(directions)) :
-#                     if len(word_list) <=2 :
-                        
-#                         try :
-#                             if boggle_board[unique_all_first_position[i][0] + directions[k][0]][unique_all_first_position[i][1] + directions[k][1]] == word_list[1] :
-#                                 position = [unique_all_first_position[i][0] + directions[k][0],unique_all_first_position[i][1] + directions[k][1]]
-#                                 matches.append(position)
-#                                 print(f"first positions are : {unique_all_first_position[i][0]}, {unique_all_first_position[i][1]}")
-#                                 print(f"{word_list[1]} matches are : {matches}")
-#                             else :
-#                                 continue
-                                
-#                         except :
-#                             continue
+    # --------------------------------
+    ##### Check horizontal directions #####
+    # --------------------------------
+    def checkHorizontal() :
+        
+        match = False
+        
+        # check horizontal from left to right
+        for i in range(0, len(unique_all_first_position)) :
+            if match == True :
+                return True
+            else :    
+                for j in range(1, len(word_list)) :
+                    try :
+                        if boggle_board[unique_all_first_position[i][0]][unique_all_first_position[i][1] + j] == word_list[j] :
+                            match = True
+                        else :
+                            match = False
+                            break
                             
-#                     else :
+                    except :
+                        match = False
+                            
+        # check horizontal from right to left
+        for i in range(0, len(unique_all_first_position)) :
+            if match == True :
+                return True
+            else :    
+                for j in range(1, len(word_list)) :
+                    try:
+                        if boggle_board[unique_all_first_position[i][0]][unique_all_first_position[i][1] - j] == word_list[j] :
+                            match = True
+                        else :
+                            match = False
+                            break
+                    except: 
+                        match = False
                         
-                        
-                
-                        
-#     if matches != [] :
-#         return True
-#     else :
-#         return False
-                        
-# print(f"All matched directions: {checkAllDir()}")
+        if match == False :
+            return False
+        else :
+            return True
+    
+    
+    
+    if checkHorizontal() == True:
+        print_smarter_boggle_board()
+        print(f"You found the word horizontally !")
+        score += 1
+        print(f"Score: {score}")
+        continue
+     
 
-dictOfWords = {}
-position = []
-next_position = []
-unique_next_position = []
-combined_results = []
-first_word_length = len(word_list)
-first_word = word_list
-count = 0
 
-def checkAllDir (word, length, start) :
-    count = length
+    # --------------------------------
+    ##### Check all vertical directions #####
+    # --------------------------------
+
+    def checkVertical () :
+        match = False
+        
+        # check horizontal from left to right
+        for i in range(0, len(unique_all_first_position)) :
+            if match == True :
+                return True
+            else :    
+                for j in range(1, len(word_list)) :
+                    try :
+                        if boggle_board[unique_all_first_position[i][0] + j][unique_all_first_position[i][1]] == word_list[j] :
+                            match = True
+                        else :
+                            match = False
+                            break
+                            
+                    except :
+                        match = False
+                            
+        # check horizontal from right to left
+        for i in range(0, len(unique_all_first_position)) :
+            if match == True :
+                return True
+            else :    
+                for j in range(1, len(word_list)) :
+                    try:
+                        if boggle_board[unique_all_first_position[i][0] - j][unique_all_first_position[i][1]] == word_list[j] :
+                            match = True
+                        else :
+                            match = False
+                            break
+                    except: 
+                        match = False
+                        
+        if match == False :
+            return False
+        else :
+            return True
+    
+    if checkVertical() == True:
+        print_smarter_boggle_board()
+        print(f"You found the word vertically !")
+        score += 1
+        print(f"Score: {score}")
+        continue
+    
+
+
+    # --------------------------------
+    ##### Check all 8 directions #####
+    # --------------------------------
+
+    directions = [
+        [+1, +0], #down
+        [-1, +0], #up
+        [+0, +1], #right
+        [+0, -1], #left
+        [+1, +1], #down right
+        [-1, +1], #up right
+        [-1, -1], #up left
+        [+1, -1] #down left
+    ]
+
+
+    position = []
     next_position = []
     unique_next_position = []
-    
-    if length <= 1 :
-        # print(True)
-        return True
+    combined_results = []
+
+    def checkAllDir (word, length, start) :
+        count = length
+        next_position = []
+        unique_next_position = []
         
-    else:     
-        # del word[0]
-        for i in range(0, len(start)) :           
-            for j in range(0, len(directions)) :
-                # try:
-                    if boggle_board[min(3,max(0,start[i][0] + directions[j][0]))][min(3,max(0,start[i][1] + directions[j][1]))] == word[1] :
-                        position = [min(3,max(0,start[i][0] + directions[j][0])),min(3,max(0,start[i][1] + directions[j][1]))]
-                        next_position.append(position)
-                        unique_next_position = [list(item) for item in set(tuple(row) for row in next_position)]
-                        # print(f"i is {i}, j is {j}")
-                        # print(f"next_position is : {next_position}")
-                        # print(f"Start position {start[i]} letter {boggle_board[start[i][0]][start[i][1]]} next letter {word[1]}  is {unique_next_position}")
-                        
-                # except :
-                #     print(direction[j])
-                #     print(f"Start position {start[i]} letter {boggle_board[start[i][0]][start[i][1]]} next letter {word[1]}  is {unique_next_position}")
-                #     continue
+        if length <= 1 :
+            return True
+        else:     
+            for i in range(0, len(start)) :           
+                for j in range(0, len(directions)) :
+                    
+                        if boggle_board[min(3,max(0,start[i][0] + directions[j][0]))][min(3,max(0,start[i][1] + directions[j][1]))] == word[1] :
+                            position = [min(3,max(0,start[i][0] + directions[j][0])),min(3,max(0,start[i][1] + directions[j][1]))]
+                            next_position.append(position)
+                            unique_next_position = [list(item) for item in set(tuple(row) for row in next_position)]
             
-            # try:
-            #     print(word)
-            #     del word[1]
-            #     print(word)
-                
-            # except: 
-            #     print(word)
-                
-                # unique_next_position = [list(item) for item in set(tuple(row) for row in next_position)]
-                
-                # if unique_next_position == [] :
-                #     continue
-                
-            # if unique_next_position != [] :
-            #     combined_results.append(f"{start[i]} letter {word[1]} : True")
-            #     # print(unique_next_position)
-            #     # print(combined_results)
-                
-            # else :
-            #     # print(f"Start position {start[i]} letter {boggle_board[start[i][0]][start[i][1]]} next letter {word[1]}  is {unique_next_position}")
-            #     combined_results.append(f"{start[0]} letter {word[1]} : False")
-                
-                # print(unique_next_position)
-                # print(combined_results)
-        
-        del word[0]    
-        
-        if unique_next_position != [] :
-            if checkAllDir(word, len(word), unique_next_position) == True :
-                return True
-            else:
-                return False
+            del word[0]    
             
-        return False
-    
-if checkAllDir(word_list, len(word_list), unique_all_first_position) == False:
-    print(f"{False} !")
-
-else:
-    print(f"{True} !")
-
-# checkAllDir(word_list, len(word_list), unique_all_first_position)
-
-# print(checkAllDir(word_list, len(word_list), unique_all_first_position))
-
-
+            if unique_next_position != [] :
+                if checkAllDir(word, len(word), unique_next_position) == True :
+                    return True
+                else:
+                    return False
+                
+            return False
     
     
+    print_smarter_boggle_board()
+    
+    if checkAllDir(word_list, len(word_list), unique_all_first_position) == False:
+        print(f"DEH! INCORRECT!")
+        print(f"Score: {score}")
+
+    else:
+        print(f"You found the word ! (somehow...)")
+        score += 1
+        print(f"Score: {score}")
+
+
+
+
+        
+        
